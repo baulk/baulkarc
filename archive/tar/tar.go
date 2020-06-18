2b4ea6495774e695369db86ca632a0742df97712
Force.Charlie <charlieio@outlook.com>
2020-06-18 22:31:45 +0800

    tar cleanup code

--- archive/tar/tar.go
+++ archive/tar/tar.go
@@ -49,20 +49,20 @@ func (e *Extractor) Extract(destination string) error {
 	return nil
 }
 
-// BrewingExtractor todo
-type BrewingExtractor struct {
-	fd  *os.File
-	r   *tar.Reader
-	mwr io.ReadCloser
-	es  *rules.ExtractSetting
-}
-
 // Matched todo
 func Matched(buf []byte) bool {
 	return len(buf) > 261 &&
 		buf[257] == 0x75 && buf[258] == 0x73 &&
 		buf[259] == 0x74 && buf[260] == 0x61 &&
 		buf[261] == 0x72
+}
+
+// BrewingExtractor todo
+type BrewingExtractor struct {
+	fd  *os.File
+	r   *tar.Reader
+	mwr io.ReadCloser
+	es  *rules.ExtractSetting
 }
 
 // MatchExtension todo
